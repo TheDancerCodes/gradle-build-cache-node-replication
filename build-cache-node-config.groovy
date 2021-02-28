ff8e90f2254ca7a5e6b41994f7dc104552ff2efd
Taracha <rojtaracha@gmail.com>
2021-02-28 09:21:56 +0300

    Build Cache Node Replication: * Incorporate gradle task that generates the build cache node file ~ build-cache-node-config.properties
    * Add logic to settings.gradle.
    
    * Ensure build-cache-node-config.properties is added to .gitignore
    
    * Add tasks specific to each node region that take care of passing the correct node value.
    
    * All the logic above enables the selection of the right build cache node for a dev's region.

---
+++ build-cache-node-config.groovy
@@ -0,0 +1,21 @@
+environments {
+    default_node {
+        nodeTitle = '# Default Build Cache Node'
+        nodeUrl = 'https://gradle-enterprise.your-domain.com/cache/'
+    }
+
+    east_africa_node {
+        nodeTitle = '# East Africa Build Cache Node'
+        nodeUrl = 'https://east.xx.yyy.zzz/cache/'
+    }
+
+    west_africa_node {
+        nodeTitle = '# West Africa Build Cache Node'
+        nodeUrl = 'https://west.xx.yyy.zzz/cache/'
+    }
+
+    us_west_node {
+        nodeTitle = '# US West Build Cache Node'
+        nodeUrl = 'https://us-west.xx.yyy.zzz/cache/'
+    }
+}
